ed5b53526d80536c9d892af0a526f5aa0fc00395
Mathew Fitzgerald <mathewfitzgerald00@gmail.com>
2022-04-30 20:11:29 +0100

    Change name of pos/neg classifier to classifier.py

--- classifier.py
+++ classifier.py
@@ -1,57 +1,77 @@
 from dotenv import load_dotenv
+from pathlib import Path
 import os
 import math
+import re
+
+# Creating the Model
+# - Input 2 directories - one containing positive samples, one containing negative samples
+# - Input the path to the file containing stopwords to remove from the samples
+
+# Using the Model
+# - Load some text and clean/tokenise it using the tokenise method
+# - Input the tokens into the predict method, it will return a label
+
 
 class Model:
-    """A naive-bayes model of IMDB reviews"""
+    """A naive-bayes model trained on the set of IMDB reviews"""
 
-    def __init__(self, dataPath, vocabPath, alpha=1):
-        """Constructs a multinomial naive-bayes model using a Bag of Words approach. dataPath - the path to the bag
-        of words in LIBSVM format. vocabPath - the path to the text file containing a new-line-delineated list of words.
-        alpha - The value for smoothing, default is 1"""
+    def __init__(self, posDir, negDir, stopWordsPath, alpha=1):
+        """Constructs a multinomial naive-bayes model using a Bag of Words approach with logarithmic probability and stopwords. 
+        posDir - A directory containing the positive samples.
+        negDir - A directory containing the negative samples.
+        stopWordsPath - A path to a file containing a list of stop words to remove from samples.
+        alpha - The value for smoothing, default is 1 (laplace smoothing)"""
+
+        # Read the list of stop words from a file
+        self.stopWords = set()  # A set of stopwords
+        with open(stopWordsPath) as f:
+            contents = f.readlines()
+        for line in contents:
+            self.stopWords.add(line.strip())
 
         self.alpha = alpha  # The value to add to apply smoothing
-        self.x = countLinesInFile(vocabPath)  # The number of unique words in the vocabulary
-        self.totalClasses = {k:0 for k in range(1,11)}  # Total number of samples in each class (1-10)
-        self.wordTotals = {k:dict() for k in range(1,11)}  # A histogram of the frequency of words in each class (When calculateProbabilities is called, frequencies are replaced by probabilities)
-        #self.wordProbabilities = {k:dict() for k in range(1,11)}  # A dictionary of the probabilities of finding words in each class
-        self.zeroFrequencyProbabilities = {k:0 for k in range(1,11)}  # The probability of seeing a word in a class that didn't occur in the training set, adjusted for smoothing, for each class
-        self.vocab = set()  # A set of all word IDs found in the test samples
-
-        # Could just have a method for getting this value - add all the self.totalClasses values?
+        self.totalClasses = dict()  # Total number of samples in each class (className:frequency)
+        self.wordTotals = dict()  # (className:dict()) A histogram of the frequency of words in each class (When calculateProbabilities is called, frequencies are replaced by probabilities)
+        self.zeroFrequencyProbabilities = dict()  # The probability of seeing a word in a class that didn't occur in the training set, adjusted for smoothing, for each class
+        self.vocab = set()  # A set of all words found in the test samples, minus the stop words
         self.total = 0  # Total number of samples
 
-        with open(dataPath) as f:
-            content = f.readlines()
+        # Incrementally add each training sample to the model by adding it to the frequency histogram
+        for child in Path(posDir).iterdir():  # positive samples (7 - 10)
+            if child.is_file():
+                text = child.read_text()
+                tokens = tokenise(text, self.stopWords)
+                self.__addToModel('positive', tokens)
         
-        for line in content:
-            s = Sample(line)
-            
-            self.__addToModel(s)
+        for child in Path(negDir).iterdir():  # negative samples (0 - 4)
+            if child.is_file():
+                text = child.read_text()
+                tokens = tokenise(text, self.stopWords)
+                self.__addToModel('negative', tokens)
         
+        self.x = len(self.vocab)  # The number of unique words in the vocabulary
+        
+        # Convert the histogram to a mapping from words to probabilities
         self.__calculateProbabilities()
         
     
     def __calculateProbabilities(self):
-        """Calculates the probabilities of seeing words given a class of sample. alpha - the 
-        value to add to probabilities to apply smoothing, default is 1 (laplace smoothing)"""
+        """Calculates the probabilities of seeing words in each sample given the training data set"""
 
         for class_, histogram in self.wordTotals.items():
 
-            print("Class %d:" % (class_))
             totalWordsInClass = 0
             for freq in histogram.values():
                 totalWordsInClass += freq
-            print("total words: %d" % (totalWordsInClass))
             
             # Adjust the total word count for alpha
             adjustedTotalWordsInClass = totalWordsInClass + self.alpha * self.x
-            print("adjusted total words after smoothing: %d" % (adjustedTotalWordsInClass))
 
-            for wordID in histogram.keys():
-                freq = histogram[wordID]
+            for word in histogram.keys():
+                freq = histogram[word]
                 adjustedFreq = freq + self.alpha
-                histogram[wordID] = adjustedFreq / adjustedTotalWordsInClass
+                histogram[word] = adjustedFreq / adjustedTotalWordsInClass
             
             # Calculate the probability of a word that doesnt appear in the class
 
@@ -59,41 +79,44 @@ class Model:
             probZeroFreq = self.alpha / adjustedTotalWordsInClass  # P of seeing a single non-frequency word
             self.zeroFrequencyProbabilities[class_] = probZeroFreq
             sumOfZeroFreqs = probZeroFreq * numOfZeroFreqs  # Sum of all those probabilities (Should be 1 - the sum of P of all words in the class)
-            print("Number of words that don't appear: %d" % (numOfZeroFreqs))
-            print("Probability of seeing a zero-frequency word: %.15f" % (probZeroFreq))
-            
             sumOfProbabilities = 0
             for p in histogram.values():
                 sumOfProbabilities += p
             sumOfProbabilities += sumOfZeroFreqs
-            print("sum of probabilities: %.5f" % (sumOfProbabilities), end="\n\n")  # Should be 1
 
     
-    def __addToModel(self, sample):
-        """Adds a single Sample to the model. A sample consists of a label, and a bag of words feature vector"""
+    def __addToModel(self, label, tokens):
+        """Adds a single sample to the model. A sample consists of a label, and tokens - a bag of words feature vector"""
 
-        # How to add:
-        # self.wordTotals (
-        #   Key: class
-        #   Value: dict (
-        #       Key: wordID
-        #       Value: frequency
+        for word, freq in tokens.items():
+            self.vocab.add(word)  # Add the word to the vocab
 
-        for wordID, freq in sample.features.items():
-            self.vocab.add(wordID)  # Add the word ID to the vocab
-            if wordID not in self.wordTotals[sample.label]:
-                self.wordTotals[sample.label][wordID] = freq
+            if label not in self.wordTotals:
+                self.wordTotals[label] = dict()
+
+            if word not in self.wordTotals[label]:
+                self.wordTotals[label][word] = freq
             else:
-                self.wordTotals[sample.label][wordID] += freq
+                self.wordTotals[label][word] += freq
         
         self.total += 1
-        self.totalClasses[sample.label] += 1
+
+        if label not in self.totalClasses:
+            self.totalClasses[label] = 1
+        else:
+            self.totalClasses[label] += 1
+        
+        if label not in self.zeroFrequencyProbabilities:
+            self.zeroFrequencyProbabilities[label] = 0
     
 
-    def predict(self, sample):
-        """Predicts the class (1-10) of a review using naive-bayes"""
+    def predict(self, tokens):
+        """Predicts the class of a sample in tokenised form"""
         
-        probabilities = [self.getProbability(sample, i) for i in range(1, 11)]
+        classes = [i for i in self.totalClasses.keys()]
+        classes.sort()
+
+        probabilities = [self.getProbability(tokens, i) for i in classes]
 
         maxP = probabilities[0]
         maxI = 0
@@ -102,88 +125,43 @@ class Model:
                 maxI = i
                 maxP = probabilities[i]
         
-        return maxI + 1  # Return the number of class, not the index (which is index + 1)
+        return classes[maxI]  # Return the class with the highest probability
 
     
-    def getProbability(self, sample, i):
-        """Calculates the probability of the sample being in the class i"""
-        try:
-            pClass = math.log(self.totalClasses[i] / self.total)  # The prior probability of the sample being in class i
-        except:
-            pClass = 0
+    def getProbability(self, tokens, i):
+        """Calculates the probability of a sample being in the class i"""
+        
+        pClass = math.log(self.totalClasses[i] / self.total)  # The prior probability of the sample being in class i
         totalP = 0
 
-        # Multiply totalP by the probability of each word in the sample being in class i
-        for wordID, freq in sample.features.items():
+        # Increase totalP by the probability of each word in the sample being in class i
+        for word, freq in tokens.items():
             for j in range(freq):
-                totalP += math.log(self.wordTotals[i].get(wordID, self.zeroFrequencyProbabilities[i]))
-            #totalP *= math.pow(self.wordTotals[i].get(wordID, self.zeroFrequencyProbabilities[i]), freq)
+                totalP += math.log(self.wordTotals[i].get(word, self.zeroFrequencyProbabilities[i]))
 
-        totalP *= pClass
+        totalP += pClass  # The final posterior probability
         return totalP
 
 
-class Sample:
-    """Represents a single review as a bag of words with a label"""
+def tokenise(text, stopWords):
+    """Cleans and tokenises a piece of text"""
 
-    def __init__(self, line):
-        """Creates a sample from a line of text from the LIBSVM bag of words file, where label - an integer 
-        score from 1 to 10. features - a dictionary of wordID (int, >= 0) to frequency (int, >= 1)."""
+    tokens = dict()
+    text = re.sub(r"<.*>",'', text.lower())  # Remove html tags
 
-        label, values = line.split(' ', 1)
-        self.label = int(label)
-        self.features = dict()
-        
-        for e in values.split():
-            wordID, freq = e.split(':')
-            self.features[int(wordID)] = int(freq)
-    
-    
-    def getLabel(self):
-        return self.label
-    
-    def getFeatures(self):
-        return self.features
-    
-    def getFreq(self, wordID):
-        if not wordID in self.features:
-            return 0
+    # Split into words and remove apostrophe from plurals
+    words = [x.replace("'s", "s") for x in re.findall(r"[\w'-]+", text)]
+
+    # Remove stop words
+    cleanedWords = [x for x in words if x not in stopWords]
+
+    for word in cleanedWords:
+        if word in tokens:
+            tokens[word] += 1
         else:
-            return self.features[wordID]
-    
-    def __str__(self):
-        s = "%d" % (self.label)
-        for wordID, freq in self.features.items():
-            s += " %d:%d" % (wordID, freq)
-        s += '\n'
-        return s
+            tokens[word] = 1
 
-
-def countLinesInFile(path):
-    """Reads a file and returns the number of non-blank lines"""
-    x = 0
-
-    with open(path) as f:
-        c = f.readlines()
-        for line in c:
-            if len(line.strip()) != 0:
-                x += 1
-    
-    return x
-
-
-def readBOW(path):
-    """Reads the bag of words file in LIBSVM format (<label> <word ID>:<frequency>) and puts it in a list"""
-    
-    samples = list()
-
-    with open(path) as f:
-        content = f.readlines()
-    
-    for line in content:
-        samples.append(Sample(line))
-    
-    return samples
+    return tokens
 
 
 if __name__ == "__main__":
@@ -191,34 +169,69 @@ if __name__ == "__main__":
     load_dotenv()  # take environment variables from .env.
     
     # Create the model
-    model = Model(os.environ['TRAINDATA'], os.environ['VOCABFILE'])
+    model = Model(os.environ['TRAINPOSDIR'], os.environ['TRAINNEGDIR'], os.environ['STOPWORDS'], 1)
     print("Number of unique words in all samples: %d" % (len(model.vocab)))
 
     # Test Accuracy
+    numberOfTestClasses = dict()
 
-    # Get the test data
-    samples = readBOW(os.environ['TESTDATA'])
     numCorrect = 0
+    numPredictedPos = 0
+    numPredictedNeg = 0
+    numFalsePositives = 0
+    numFalseNegatives = 0
     total = 0
-    withinX = dict()  # Keys: difference, values: frequency
 
-    for sample in samples:
-        predictedClass = model.predict(sample)
-        actualClass = sample.getLabel()
-        if predictedClass == actualClass:
-            numCorrect += 1
-        total += 1
+    stopWords = set()
+    with open(os.environ['STOPWORDS']) as f:
+        contents = f.readlines()
+    for line in contents:
+        stopWords.add(line.strip())
 
-        # Find difference
-        diff = abs(predictedClass - sample.getLabel())
-        if diff in withinX:
-            withinX[diff] += 1
-        else:
-            withinX[diff] = 1
+    testDirs = [(os.environ['TESTPOSDIR'], 'positive'), (os.environ['TESTNEGDIR'], 'negative')]
+    for directory in testDirs:
+        for child in Path(directory[0]).iterdir():
+            if child.is_file():
+                label = directory[1]
+                text = child.read_text()
+                tokens = tokenise(text, stopWords)
+
+                # Count the test sample
+                if label in numberOfTestClasses:
+                    numberOfTestClasses[label] += 1
+                else:
+                    numberOfTestClasses[label] = 1
+
+                # Predict
+                predictedClass = model.predict(tokens)
+                total += 1
+                if predictedClass == label:
+                    numCorrect += 1
+                else:
+                    if predictedClass == 'positive':
+                        numFalsePositives += 1
+                    else:
+                        numFalseNegatives += 1
+                if predictedClass == 'positive':
+                    numPredictedPos += 1
+                else:
+                    numPredictedNeg += 1
     
     accuracy = (numCorrect / total) * 100
 
-    print("Accuracy: %.2f" % (accuracy))
-    print("Differences:")
-    for k,v in withinX.items():
-        print("%d : %.2f" % (k, (v / total) * 100))
+    print("Accuracy: %.2f\n" % (accuracy))
+    print("Accuracy when sample is positive: %.2f" % (((numPredictedNeg - numFalsePositives) / 12500) * 100))
+    print("Accuracy when sample is negative: %.2f" % (((numPredictedPos - numFalseNegatives) / 12500) * 100))
+    
+    print("Number of samples in different classes:")
+    for k,v  in numberOfTestClasses.items():
+        print("%s : %d" % (k, v))
+    
+    print("Number of predicted positive samples: %d (%.2f)" % (numPredictedPos, ((numPredictedPos / total) * 100)))
+    print("Number of correct positives: %d (%.2f)" % (numPredictedPos - numFalsePositives, (((numPredictedPos - numFalsePositives) / total) * 100)))
+    print("Number of false positives: %d (%.2f)" % (numFalsePositives, ((numFalsePositives / total) * 100)))
+    print("Number of predicted negative samples: %d (%.2f)" % (numPredictedNeg, ((numPredictedNeg / total) * 100)))
+    print("Number of correct negatives: %d (%.2f)" % (numPredictedNeg - numFalseNegatives, (((numPredictedNeg - numFalseNegatives) / total) * 100)))
+    print("Number of false negatives: %d (%.2f)" % (numFalseNegatives, ((numFalseNegatives / total) * 100)))
+
+
